196d029c78bb14e2c38fbc849fbe8fbd6a20f2dd
Michał Fereniec <michalkoks99@gmail.com>
2022-08-23 15:32:23 +0200

    s1mple View made w/ NewsViewController

---
+++ Newsly/NewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsViewController.swift
+//  Newsly
+//
+//  Created by Michal Fereniec on 22/08/2022.
+//
+
+import UIKit
+
+
+class NewsViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Newsly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newsly
+//
+//  Created by Michal Fereniec on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
